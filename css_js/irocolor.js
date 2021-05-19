a01db976fb85fbbb3c92929931661cefb86c81ea
punspuns <74218652+punspuns@users.noreply.github.com>
2021-05-20 00:35:13 +0900

    met roi

--- css_js/irocolor.js
+++ css_js/irocolor.js
@@ -1,19 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-	// Creating waypoint
-	let waypoint = new Waypoint({
-	  element: document.querySelector('#yokai'),
-	  handler: function() {
-			anime({
-				targets: '#yokai',
-				easing: 'easeOutExpo',
-				translateX: [100, 0],
-				opacity: [0, 1],
-				delay: 300,
-			})
+$(window).scroll(function() {
+    var winHeight = $(this).height();
+    var scrollTop = $(this).scrollTop();
+    
+    var elemHeight = $(".kanji").height();
+    var elementTop = $(".kanji").position().top; 
 
-			this.destroy();
-	  },
-		context: document.querySelector('.text-content'),
-		offset: '100%',
-	})
-})
+     
+    if (elementTop < scrollTop + winHeight && scrollTop > elementTop + elemHeight){
+		$("#samurai").fadeOut();
+	}
+		
+    else{
+		$("#samurai").fadeIn();
+	}
+    
+    
+});
