c8e29635310949529df6cd7a27b2f5aaf53f5fc1
punspuns <74218652+punspuns@users.noreply.github.com>
2021-05-20 11:01:24 +0900

    stuff

--- css_js/irocolor.js
+++ css_js/irocolor.js
@@ -1,18 +1,41 @@
+
 $(window).scroll(function() {
     var winHeight = $(this).height();
     var scrollTop = $(this).scrollTop();
     
-    var elemHeight = $(".kanji").height();
-    var elementTop = $(".kanji").position().top; 
+    var elemHeight = $("#kanji-1").height();
+    var elementTop = $("#kanji-1").position().top; 
+
 
      
     if (elementTop < scrollTop + winHeight && scrollTop > elementTop + elemHeight){
-		$("#samurai").fadeOut();
+		  $("#samurai").fadeOut();
+      $("#samurai").hide();
 	}
 		
     else{
-		$("#samurai").fadeIn();
+		  $("#samurai").fadeIn();
 	}
     
     
 });
+
+$(window).scroll(function() {
+  var winHeight = $(this).height();
+  var scrollTop = $(this).scrollTop();
+
+  var elemHeight2 = $("#kanji-2").height();
+  var elementTop2 = $("#kanji-2").position().top; 
+
+  if (elementTop2 < scrollTop + winHeight && scrollTop < elementTop2 + elemHeight2){
+    $("#yokai").fadeIn();
+}
+  
+  else{
+    $("#yokai").fadeOut();
+    $("#yokai").hide();
+}
+  
+  
+});
+
